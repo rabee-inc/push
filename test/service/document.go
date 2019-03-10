e4cc6d0087fc3ec5c845159bd56b221699f4b4af
aikizoku <>
2019-03-10 23:15:01 +0900

    testを削除

--- test/service/document.go
+++
@@ -1,9 +0,0 @@
-package service
-
-import "github.com/rabee-inc/push/test/model"
-
-// Document ... ドキュメントを操作する
-type Document interface {
-	RemoveAll()
-	Distributes(tmplPath string, apis []*model.API)
-}
--- test/service/jsonrpc2.go
+++
@@ -1,9 +0,0 @@
-package service
-
-import "github.com/rabee-inc/push/test/model"
-
-// JSONRPC2 ... JSONRPC2形式のAPI通信を行う
-type JSONRPC2 interface {
-	Send(name string, method string, params map[string]interface{})
-	GetAPIs() []*model.API
-}
